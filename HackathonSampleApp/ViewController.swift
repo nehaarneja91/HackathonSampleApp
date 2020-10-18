58aea13e789fa0ce0b3848d33b168b59c1171b74
Kumar Singh, Randhir <rkunv@allstate.com>
2020-10-19 02:17:39 +0530

    Initial Commit

---
+++ HackathonSampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HackathonSampleApp
+//
+//  Created by Kumar Singh, Randhir on 19/10/20.
+//  Copyright © 2020 Kumar Singh, Randhir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
