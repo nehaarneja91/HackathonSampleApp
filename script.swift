c6b9309e3a5b0ccb0bf363deb1d29ab5e4f6402a
Kumar Singh, Randhir <rkunv@allstate.com>
2020-10-22 03:45:33 +0530

    modified script

--- script.swift
+++ script.swift
@@ -8,6 +8,7 @@ let scheme = "HackathonSampleAppUITests"
 let projectScheme = "HackathonSampleApp"
 let project = "HackathonSampleApp.xcodeproj"
 let platform = "platform=iOS Simulator,name=iPhone 11 Pro Max,OS=13.2.2"
+let CONFIGURATION = "Debug"
 
 func getTestPlans() -> [String] {
     let pipe = Pipe()
@@ -42,6 +43,46 @@ let testPlanList = getTestPlans()
 //xcodebuild test -project SnapShotExperiment.xcodeproj -scheme SnapShotExperimentUITests -testPlan SnapShotExperimentUITests
 //xcodebuild -project HackathonSampleApp.xcodeproj -scheme HackathonSampleAppUITests -destination 'platform=iOS Simulator,name=iPhone 11 Pro Max,OS=13.2.2' test
 
+
+func makeScreenshotDir() {
+    let process = Process()
+    process.executableURL = URL(fileURLWithPath: "/usr/bin/env")
+    process.arguments = ["mkdir", "screenshots"]
+    do {
+        print("--------------Making screenshots Folder--------------")
+        try process.run()
+        process.waitUntilExit()
+    } catch  {
+        print("Error while creating screenshot dir")
+    }
+}
+
+func moveScreenshots() {
+    let process = Process()
+    process.executableURL = URL(fileURLWithPath: "/usr/bin/env")
+    process.arguments = ["mv", "DerivedData/Build/Products/\(CONFIGURATION)-iphonesimulator/screenshots", "."]
+    do {
+        print("--------------Moving screenshots--------------")
+        try process.run()
+        process.waitUntilExit()
+    } catch  {
+        print("Error while moving screenshot")
+    }
+}
+
+func removeDerivedDataDir() {
+    let process = Process()
+    process.executableURL = URL(fileURLWithPath: "/usr/bin/env")
+    process.arguments = ["rm", "-r", "DerivedData"]
+    do {
+        print("--------------Removing DerivedData--------------")
+        try process.run()
+        process.waitUntilExit()
+    } catch  {
+        print("Error while removing  DerivedData")
+    }
+}
+
 func runTest() {
     let process = Process()
     process.executableURL = URL(fileURLWithPath: "/usr/bin/env")
@@ -52,24 +93,23 @@ func runTest() {
         "-scheme", projectScheme,
         "-destination", platform,
         "-testPlan", testPlanList[1],
+        "-derivedDataPath", "DerivedData",
         "test"
     ]
     
     do {
         try process.run()
-        
-        
+        process.waitUntilExit()
+       // makeScreenshotDir()
+        moveScreenshots()
+        removeDerivedDataDir()
     } catch  {
         print("Error while testing")
     }
 }
-
-func moveScreenshots() {
-    let process = Process()
-    process.executableURL = URL(fileURLWithPath: "/usr/bin/env")
-}
-//mkdir simulatorBuild
+print("--------------Available Schemes--------------")
 print(testPlanList)
 runTest()
+
 
 
